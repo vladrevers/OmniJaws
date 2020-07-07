ed8b934ef9b8aef5805aeaff5b1ea3195c4a1c9b
vladrevers <vlad.k.o.vl66@gmail.com>
2020-07-07 18:19:15 +0300

    TWC: fix use of default values for location info (again..)

--- app/src/main/java/org/omnirom/omnijaws/TheWeatherChannelProvider.java
+++ app/src/main/java/org/omnirom/omnijaws/TheWeatherChannelProvider.java
@@ -200,8 +200,8 @@ public class TheWeatherChannelProvider extends AbstractWeatherProvider {
 
         try {
             JSONObject jsonResults = new JSONObject(response).getJSONObject("location");
-            keyAndName[0] = jsonResults.optString("placeId", defaultCityId);
-            keyAndName[1] = jsonResults.optString("city", defaultCityName);
+            keyAndName[0] = jsonResults.isNull("placeId") ? defaultCityId : jsonResults.getString("placeId");
+            keyAndName[1] = jsonResults.isNull("city") ? defaultCityName : jsonResults.getString("city");
         } catch (JSONException e) {
             Log.e(TAG, "Received malformed location info ("
                     + partLocation + ", lang=" + getLanguage() + ")", e);
