02a40eaa0a5f50978894908ac6fa22207018074b
maxwen <max.weninger@gmail.com>
2022-04-02 02:38:49 +0300

    OWM: switch to onecall API

--- app/src/main/java/org/omnirom/omnijaws/OpenWeatherMapProvider.java
+++ app/src/main/java/org/omnirom/omnijaws/OpenWeatherMapProvider.java
@@ -16,42 +16,44 @@
 
 package org.omnirom.omnijaws;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
+import android.content.Context;
+import android.content.res.Resources;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.net.Uri;
+import android.text.TextUtils;
+import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.omnirom.omnijaws.WeatherInfo.DayForecast;
 
-import android.content.Context;
-import android.content.res.Resources;
-import android.location.Location;
-import android.net.Uri;
-import android.text.TextUtils;
-import android.util.Log;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 public class OpenWeatherMapProvider extends AbstractWeatherProvider {
     private static final String TAG = "OpenWeatherMapProvider";
 
     private static final int FORECAST_DAYS = 5;
-    private static final String SELECTION_LOCATION = "lat=%f&lon=%f";
-    private static final String SELECTION_ID = "id=%s";
-
-    private static final String URL_LOCATION =
-            "http://api.openweathermap.org/data/2.5/find?q=%s&mode=json&lang=%s&appid=%s";
+    private static final String PART_COORDINATES = "lat=%f&lon=%f";
     private static final String URL_WEATHER =
-            "http://api.openweathermap.org/data/2.5/weather?%s&mode=json&units=%s&lang=%s&appid=%s";
-    private static final String URL_FORECAST =
-            "http://api.openweathermap.org/data/2.5/forecast?%s&mode=json&units=%s&lang=%s&cnt=" + FORECAST_DAYS + "&appid=%s";
+            "https://api.openweathermap.org/data/2.5/onecall?%s&mode=json&units=%s&lang=%s&cnt=" + FORECAST_DAYS + "&appid=%s";
+    private static final String URL_PLACES =
+            "http://api.geonames.org/searchJSON?q=%s&lang=%s&username=omnijaws&maxRows=20";
+    private static final String URL_LOCALITY =
+            "http://api.geonames.org/extendedFindNearbyJSON?lat=%f&lng=%f&lang=%s&username=omnijaws";
+    private static final SimpleDateFormat dayFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
 
-    private List<String> mKeys = new ArrayList<String>();
-    private boolean mHasAPIKey;
+    private final List<String> mKeys = new ArrayList<>();
+    private final boolean mHasAPIKey;
     private int mRequestNumber;
 
     public OpenWeatherMapProvider(Context context) {
@@ -61,35 +63,37 @@ public class OpenWeatherMapProvider extends AbstractWeatherProvider {
     }
 
     public List<WeatherInfo.WeatherLocation> getLocations(String input) {
-        if (!mHasAPIKey) {
-            return null;
-        }
-        mRequestNumber++;
-        String url = String.format(URL_LOCATION, Uri.encode(input), getLanguageCode(), getAPIKey());
+        String lang = Locale.getDefault().getLanguage().replaceFirst("_", "-");
+        String url = String.format(URL_PLACES, Uri.encode(input), lang);
         String response = retrieve(url);
         if (response == null) {
             return null;
         }
-
         log(TAG, "URL = " + url + " returning a response of " + response);
 
         try {
-            JSONArray jsonResults = new JSONObject(response).getJSONArray("list");
-            ArrayList<WeatherInfo.WeatherLocation> results = new ArrayList<WeatherInfo.WeatherLocation>();
+            JSONArray jsonResults = new JSONObject(response).getJSONArray("geonames");
+            ArrayList<WeatherInfo.WeatherLocation> results = new ArrayList<>(jsonResults.length());
             int count = jsonResults.length();
 
             for (int i = 0; i < count; i++) {
                 JSONObject result = jsonResults.getJSONObject(i);
-                WeatherInfo.WeatherLocation location = new WeatherInfo.WeatherLocation();
+                String city = result.getString("name");
 
-                location.id = result.getString("id");
-                location.city = result.getString("name");
-                location.countryId = result.getJSONObject("sys").getString("country");
+                WeatherInfo.WeatherLocation location = new WeatherInfo.WeatherLocation();
+                String area = result.getString("adminName1");
+                location.id = String.format(Locale.US, PART_COORDINATES, result.getDouble("lat"), result.getDouble("lng"));
+                location.city = city;
+                if (!TextUtils.isEmpty(area)) {
+                    location.countryId = city.equals(area) ? result.getString("countryName") : result.getString("countryName") + ", " + area;
+                } else {
+                    location.countryId = result.getString("countryName");
+                }
                 results.add(location);
             }
 
             return results;
-        } catch (JSONException e) {
+        } catch (Exception e) {
             Log.w(TAG, "Received malformed location data (input=" + input + ")", e);
         }
 
@@ -97,14 +101,12 @@ public class OpenWeatherMapProvider extends AbstractWeatherProvider {
     }
 
     public WeatherInfo getCustomWeather(String id, boolean metric) {
-        String selection = String.format(Locale.US, SELECTION_ID, id);
-        return handleWeatherRequest(selection, metric);
+        return handleWeatherRequest(id, metric);
     }
 
     public WeatherInfo getLocationWeather(Location location, boolean metric) {
-        String selection = String.format(Locale.US, SELECTION_LOCATION,
-                location.getLatitude(), location.getLongitude());
-        return handleWeatherRequest(selection, metric);
+        String coordinates = String.format(Locale.US, PART_COORDINATES, location.getLatitude(), location.getLongitude());
+        return handleWeatherRequest(coordinates, metric);
     }
 
     private WeatherInfo handleWeatherRequest(String selection, boolean metric) {
@@ -121,35 +123,27 @@ public class OpenWeatherMapProvider extends AbstractWeatherProvider {
         }
         log(TAG, "Condition URL = " + conditionUrl + " returning a response of " + conditionResponse);
 
-        mRequestNumber++;
-        String forecastUrl = String.format(Locale.US, URL_FORECAST, selection, units, locale, getAPIKey());
-        String forecastResponse = retrieve(forecastUrl);
-        if (forecastResponse == null) {
-            return null;
-        }
-        log(TAG, "Forcast URL = " + forecastUrl + " returning a response of " + forecastResponse);
-
         try {
             JSONObject conditions = new JSONObject(conditionResponse);
-            JSONObject weather = conditions.getJSONArray("weather").getJSONObject(0);
-            JSONObject conditionData = conditions.getJSONObject("main");
-            JSONObject windData = conditions.getJSONObject("wind");
+            JSONObject conditionData = conditions.getJSONObject("current");
+            JSONObject weather = conditionData.getJSONArray("weather").getJSONObject(0);
             ArrayList<DayForecast> forecasts =
-                    parseForecasts(new JSONObject(forecastResponse).getJSONArray("list"), metric);
-            String localizedCityName = conditions.getString("name");
-            float windSpeed = (float) windData.getDouble("speed");
+                    parseForecasts(conditions.getJSONArray("daily"), metric);
+            float windSpeed = (float) conditionData.getDouble("wind_speed");
             if (metric) {
                 // speeds are in m/s so convert to our common metric unit km/h
                 windSpeed *= 3.6f;
             }
-            WeatherInfo w = new WeatherInfo(mContext, conditions.getString("id"), localizedCityName,
+
+            String city = getWeatherDataLocality(selection);
+
+            WeatherInfo w = new WeatherInfo(mContext, selection, city,
                     /* condition */ weather.getString("main"),
-                    /* conditionCode */ mapConditionIconToCode(
-                            weather.getString("icon"), weather.getInt("id")),
+                    /* conditionCode */ mapConditionIconToCode(weather.getInt("id")),
                     /* temperature */ sanitizeTemperature(conditionData.getDouble("temp"), metric),
                     /* humidity */ (float) conditionData.getDouble("humidity"),
                     /* wind */ windSpeed,
-                    /* windDir */ windData.has("deg") ? windData.getInt("deg") : 0,
+                    /* windDir */ conditionData.has("WIND_deg") ? conditionData.getInt("wind_deg") : 0,
                     metric,
                     forecasts,
                     System.currentTimeMillis());
@@ -165,25 +159,25 @@ public class OpenWeatherMapProvider extends AbstractWeatherProvider {
     }
 
     private ArrayList<DayForecast> parseForecasts(JSONArray forecasts, boolean metric) throws JSONException {
-        ArrayList<DayForecast> result = new ArrayList<DayForecast>();
+        ArrayList<DayForecast> result = new ArrayList<>();
         int count = forecasts.length();
 
         if (count == 0) {
             throw new JSONException("Empty forecasts array");
         }
         for (int i = 0; i < count; i++) {
+            String day = getDay(i);
             DayForecast item = null;
             try {
                 JSONObject forecast = forecasts.getJSONObject(i);
-                JSONObject conditionData = forecast.getJSONObject("main");
+                JSONObject conditionData = forecast.getJSONObject("temp");
                 JSONObject data = forecast.getJSONArray("weather").getJSONObject(0);
                 item = new DayForecast(
-                        /* low */ sanitizeTemperature(conditionData.getDouble("temp_min"), metric),
-                        /* high */ sanitizeTemperature(conditionData.getDouble("temp_max"), metric),
+                        /* low */ sanitizeTemperature(conditionData.getDouble("min"), metric),
+                        /* high */ sanitizeTemperature(conditionData.getDouble("max"), metric),
                         /* condition */ data.getString("main"),
-                        /* conditionCode */ mapConditionIconToCode(
-                                data.getString("icon"), data.getInt("id")),
-                        "NaN",
+                        /* conditionCode */ mapConditionIconToCode(data.getInt("id")),
+                        day,
                         metric);
             } catch (JSONException e) {
                 Log.w(TAG, "Invalid forecast for day " + i + " creating dummy", e);
@@ -211,7 +205,6 @@ public class OpenWeatherMapProvider extends AbstractWeatherProvider {
                 result.add(item);
             }
         }
-
         return result;
     }
 
@@ -231,7 +224,7 @@ public class OpenWeatherMapProvider extends AbstractWeatherProvider {
         return (float) value;
     }
 
-    private static final HashMap<String, String> LANGUAGE_CODE_MAPPING = new HashMap<String, String>();
+    private static final HashMap<String, String> LANGUAGE_CODE_MAPPING = new HashMap<>();
     static {
         LANGUAGE_CODE_MAPPING.put("bg-", "bg");
         LANGUAGE_CODE_MAPPING.put("de-", "de");
@@ -263,7 +256,7 @@ public class OpenWeatherMapProvider extends AbstractWeatherProvider {
         return "en";
     }
 
-    private int mapConditionIconToCode(String icon, int conditionId) {
+    private int mapConditionIconToCode(int conditionId) {
 
         // First, use condition ID for specific cases
         switch (conditionId) {
@@ -357,6 +350,97 @@ public class OpenWeatherMapProvider extends AbstractWeatherProvider {
         }
 
         return -1;
+    }
+
+    protected String getWeatherDataLocality(String coordinates) {
+        String city;
+        if (Config.isCustomLocation(mContext)) {
+            city = Config.getLocationName(mContext);
+            if (TextUtils.isEmpty(city)) {
+                city = getCoordinatesLocality(coordinates);
+            }
+        } else {
+            city = getCoordinatesLocality(coordinates);
+        }
+        if (TextUtils.isEmpty(city)) {
+            city = mContext.getResources().getString(R.string.omnijaws_city_unknown);
+        }
+        log(TAG, "getWeatherDataLocality = " + city);
+        return city;
+    }
+
+    private String getCoordinatesLocality(String coordinate) {
+        String cityGoogle = getCoordinatesLocalityWithGoogle(coordinate);
+
+        if (!TextUtils.isEmpty(cityGoogle)) {
+            return cityGoogle;
+        }
+
+        double latitude = Double.valueOf(coordinate.substring(4, coordinate.indexOf("&")));
+        double longitude = Double.valueOf(coordinate.substring(coordinate.indexOf("lon=") + 4));
+        Log.d(TAG, "getCoordinatesLocality " + latitude + " " + longitude);
+
+        String lang = Locale.getDefault().getLanguage().replaceFirst("_", "-");
+        String url = String.format(Locale.US, URL_LOCALITY, latitude, longitude, lang);
+        String response = retrieve(url);
+        if (response == null) {
+            return null;
+        }
+        log(TAG, "URL = " + url + " returning a response of " + response);
+
+        try {
+            JSONObject jsonResults = new JSONObject(response);
+            if (jsonResults.has("address")) {
+                JSONObject address = jsonResults.getJSONObject("address");
+                String city = address.getString("placename");
+                String area = address.getString("adminName2");
+                if (!TextUtils.isEmpty(city)) {
+                    return city;
+                }
+                if (!TextUtils.isEmpty(area)) {
+                    return area;
+                }
+            } else if (jsonResults.has("geonames")) {
+                JSONArray jsonResultsArray = jsonResults.getJSONArray("geonames");
+                int count = jsonResultsArray.length();
+
+                for (int i = count - 1; i >= 0; i--) {
+                    JSONObject geoname = jsonResultsArray.getJSONObject(i);
+                    String name = geoname.getString("name");
+
+                    if (!TextUtils.isEmpty(name)) {
+                        return name;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Log.w(TAG, "Received malformed location data (coordinate=" + coordinate + ")", e);
+        }
+        return null;
+    }
+
+    private String getCoordinatesLocalityWithGoogle(String coordinate) {
+        double latitude = Double.valueOf(coordinate.substring(4, coordinate.indexOf("&")));
+        double longitude = Double.valueOf(coordinate.substring(coordinate.indexOf("lon=") + 4));
+        Geocoder geocoder = new Geocoder(mContext.getApplicationContext(), Locale.getDefault());
+        try {
+            List<Address> listAddresses = geocoder.getFromLocation(latitude, longitude, 1);
+            if(listAddresses != null && listAddresses.size() > 0){
+                Address a = listAddresses.get(0);
+                return TextUtils.isEmpty(a.getLocality()) ? a.getAdminArea() : a.getLocality();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private String getDay(int i) {
+        Calendar calendar = Calendar.getInstance();
+        if (i > 0) {
+            calendar.add(Calendar.DATE, i);
+        }
+        return dayFormat.format(calendar.getTime());
     }
 
     private void loadKeys() {
